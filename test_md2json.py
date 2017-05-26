504b52cf8211295898053c99576f8cb985a04bab
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2017-05-27 00:22:51 +0200

    Add test for header with attributes

--- test_md2json.py
+++ test_md2json.py
@@ -30,3 +30,27 @@ header description
 
     markdown(string_to_render)
     assert json.loads(renderer()) == header
+
+
+def test_header_without_description_with_attributes():
+    header = {"test-header": {
+        "attributes":[
+            {"Byte": '0', "Length": '4', "Desc": "Signature"},
+            {"Byte": '4', "Length": '4', "Desc": "Version"},
+            {"Byte": '8', "Length": '4', "Desc": "File size"}
+        ]}}
+    string_to_render = '''
+# test-header
+
+Byte | Length | Desc
+-----|--------|------------
+0    | 4      | Signature
+4    | 4      | Version
+8    | 4      | File size
+'''
+
+    renderer = md2json.JsonRenderer()
+    markdown = mistune.Markdown(renderer=renderer)
+
+    markdown(string_to_render)
+    assert json.loads(renderer()) == header
